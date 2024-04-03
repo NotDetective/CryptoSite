8ec11d6ed96aeeb0ab5a863faba2557b568c38f7
Micha <90092439+NotDetective@users.noreply.github.com>
2024-04-03 14:23:43 +0200

    Revert "added the serch function"

--- src/components/CryptoList.tsx
+++ src/components/CryptoList.tsx
@@ -2,9 +2,7 @@ import {useEffect, useState} from "react";
 import CryptoItem from "./CryptoItem.tsx";
 
 function cryptoList() {
-    const [apiData, setApiData] = useState([]); // Assuming data is structured like { data: [...] }
     const [cryptoList, setCryptoList] = useState([]);
-    const [search, setSearch] = useState("");
 
     useEffect(() => {
         fetch('https://api.coincap.io/v2/assets')
@@ -12,7 +10,6 @@ function cryptoList() {
             .then(data => {
                 // Extract the array of cryptocurrencies from the fetched data
                 const cryptocurrencies = data.data; // Assuming data is structured like { data: [...] }
-                setApiData(cryptocurrencies);
                 setCryptoList(cryptocurrencies);
             })
             .catch(error => {
@@ -20,43 +17,14 @@ function cryptoList() {
             });
     }, []);
 
-    useEffect(() => {
-
-        // Filter the cryptocurrencies based on the search query
-        const filteredCryptos = apiData.filter((crypto) => {
-            return crypto.name.toLowerCase().includes(search.toLowerCase());
-        });
-
-        setCryptoList(filteredCryptos);
-    }, [
-        search
-    ]);
-
     return (
-        <div
-            className={"p-4"}
-        >
-            <h1
-                className={"text-2xl font-bold text-center mb-4"}
-            >
-                Cryptocurrencies
-            </h1>
-            <input
-                className={"w-full p-2 rounded-md border border-gray-300 mb-4"}
-                type="text"
-                placeholder="Search cryptocurrencies..."
-                value={search}
-                onChange={(e) => setSearch(e.target.value)}
-            />
-            <div
-                className={"grid grid-cols-3 gap-4"}
-            >
-                {
-                    cryptoList.map((crypto) => (
-                        CryptoItem({crypto})
-                    ))
-                }
-            </div>
+        <div>
+            <h1>Cryptocurrency Prices</h1>
+            {
+                cryptoList.map((crypto) => (
+                    CryptoItem({crypto})
+                ))
+            }
         </div>
     );
 }
